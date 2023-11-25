fe3d5f6d180ab98bfde871e2cd12bb934ef16d1e
Colson <colsoncalifornication@gmail.com>
2023-11-25 20:03:10 +0545

    decorator factories

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,36 @@
 "use strict";
+/* @ Decorators */
+// Decorators function start with capital letter
+// target of this decorator is the constructor function
+/*
+function Logger(constructor: Function) {
+  console.log('Logging...');
+  // we actually see the whole class but class in the end are
+  // just syntactic sugar over constructor functions
+  // NOTE: Decorators execute when class is defined not when it is instantiated
+  // Decorators run when JS finds class defination, constructor definition not
+  // when we use that constructor function
+  console.log(constructor);
+}
+*/
+/* @ Decorator Factory */
+// decorator factory returns a decoration function that allows us to
+// configure it when we assigin it as a decorator to something
+// When we apply decorator factory, we've to execute it as a function so
+// that we execute the outer function and we attach the return value, which
+// is this inner function which is valid decorator function as a decorator to class therefore
+// this allows us to accept arguments
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-/* @ Decorators */
-// Decorators function start with capital letter
-// target of this decorator is the constructor function
-function Logger(constructor) {
-    console.log('Logging...');
-    // we actually see the whole class but class in the end are
-    // just syntactic sugar over constructor functions
-    // NOTE: Decorators execute when class is defined not when it is instantiated
-    // Decorators run when JS finds class defination, constructor definition not
-    // when we use that constructor function
-    console.log(constructor);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
 let Person = class Person {
     constructor() {
@@ -24,7 +39,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    Logger('LOGGIN - PERSON')
 ], Person);
 const per = new Person();
 console.log(per);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /* @ Decorators */
 // Decorators function start with capital letter
 // target of this decorator is the constructor function
+/*
 function Logger(constructor: Function) {
   console.log('Logging...');
   // we actually see the whole class but class in the end are
@@ -10,8 +11,23 @@ function Logger(constructor: Function) {
   // when we use that constructor function
   console.log(constructor);
 }
+*/
+/* @ Decorator Factory */
+// decorator factory returns a decoration function that allows us to
+// configure it when we assigin it as a decorator to something
+// When we apply decorator factory, we've to execute it as a function so
+// that we execute the outer function and we attach the return value, which
+// is this inner function which is valid decorator function as a decorator to class therefore
+// this allows us to accept arguments
 
-@Logger
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+@Logger('LOGGIN - PERSON')
 class Person {
   name = 'Colson';
 
