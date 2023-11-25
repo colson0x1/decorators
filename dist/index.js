d6569aea3a89dd6a68a36dbf2a57877a8c760459
Colson <colsoncalifornication@gmail.com>
2023-11-25 19:43:51 +0545

    first class decorator

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log('DECORATORS');
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+/* @ Decorators */
+// Decorators function start with capital letter
+// target of this decorator is the constructor function
+function Logger(constructor) {
+    console.log('Logging...');
+    // we actually see the whole class but class in the end are
+    // just syntactic sugar over constructor functions
+    // NOTE: Decorators execute when class is defined not when it is instantiated
+    // Decorators run when JS finds class defination, constructor definition not
+    // when we use that constructor function
+    console.log(constructor);
+}
+let Person = class Person {
+    constructor() {
+        this.name = 'Colson';
+        console.log('Creating person object...');
+    }
+};
+Person = __decorate([
+    Logger
+], Person);
+const per = new Person();
+console.log(per);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('DECORATORS');
+/* @ Decorators */
+// Decorators function start with capital letter
+// target of this decorator is the constructor function
+function Logger(constructor: Function) {
+  console.log('Logging...');
+  // we actually see the whole class but class in the end are
+  // just syntactic sugar over constructor functions
+  // NOTE: Decorators execute when class is defined not when it is instantiated
+  // Decorators run when JS finds class defination, constructor definition not
+  // when we use that constructor function
+  console.log(constructor);
+}
+
+@Logger
+class Person {
+  name = 'Colson';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const per = new Person();
+console.log(per);
